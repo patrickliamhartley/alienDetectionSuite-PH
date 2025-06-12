030126e70b13f1f248c3e839f7b0b3e363c3df3e
Patrick Hartley <patrickliamhartley@gmail.com>
2025-06-11 21:17:14 -0400

    Update NeoDataViewer.tsx

--- space-data-visualization/src/components/NeoDataViewer.tsx
+++ space-data-visualization/src/components/NeoDataViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Box, Paper, Typography, Alert, Button } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
